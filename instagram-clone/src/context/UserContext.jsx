e08c69d1f91a3c18d407aefb0ccae83e27d326aa
Ashok Dev <ashokchodipalli1@gmail.com>
2025-04-08 12:38:09 +0530

    deployed url added

--- instagram-clone/src/context/UserContext.jsx
+++ instagram-clone/src/context/UserContext.jsx
@@ -3,9 +3,9 @@ import { createContext } from "react";
 export const UserContext = createContext();
 
 const InstagramContextProvider = (props) => {
-  // const apiUrl = "https://instagram-clone-backend-rfda.onrender.com";
+  const apiUrl = "https://instagram-clone-backend-rfda.onrender.com";
 
-  const apiUrl = "http://localhost:3002";
+  // const apiUrl = "http://localhost:3002";
 
   const contextValue = {
     apiUrl,
